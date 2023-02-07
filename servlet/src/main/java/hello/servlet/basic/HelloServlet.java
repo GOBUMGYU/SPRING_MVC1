017e9d4a17e44a0965a7c58309e08dd02d06f2f1
bumgyu <soul1902@naver.com>
2023-02-07 19:08:47 +0900

    HttpServletRequest 기본 사용법

--- servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -5,6 +5,8 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 //서블릿은 HttpServlet 을 상속받아야 한다.
@@ -12,8 +14,10 @@ import java.io.IOException;
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     //HTTP 요청을 통해 URL이 호출되면 서블릿 컨테이너는 다음 메서드를 실행한다.
+
+
     @Override
-    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("HelloServlet.service");
         System.out.println("request = " + request);
         System.out.println("request = " + response);
