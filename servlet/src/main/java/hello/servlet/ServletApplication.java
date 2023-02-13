05c1a5077cd5eb4fae400b344a3d69ad0915e904
bumgyu <soul1902@naver.com>
2023-02-14 07:48:15 +0900

    스프링 MVC - 시작하기(회원저장, 조회)

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -1,11 +1,10 @@
 package hello.servlet;
 
+import hello.servlet.web.springmvc.v1.SpringMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /*
 서블릿은 톰캣 같은 웹 애플리케이션 서버를 직접 설치하고
@@ -17,4 +16,5 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @SpringBootApplication
 public class ServletApplication {
 	public static void main(String[] args) { SpringApplication.run(ServletApplication.class, args); }
+
 }
