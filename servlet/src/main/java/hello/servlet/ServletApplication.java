f521eef6920a11aed14cb5baaa73ddbbe74cb365
bumgyu <soul1902@naver.com>
2023-02-14 07:04:20 +0900

    뷰 리졸버 application.properties|prefix/suffix 설정 정보

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /*
 서블릿은 톰캣 같은 웹 애플리케이션 서버를 직접 설치하고
@@ -13,9 +16,5 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
 public class ServletApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ServletApplication.class, args);
-	}
-
+	public static void main(String[] args) { SpringApplication.run(ServletApplication.class, args); }
 }
