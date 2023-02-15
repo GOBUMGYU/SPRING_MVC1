8285fe0ad930dbaf4eb00c3f7bed15bef0d5b732
bumgyu <soul1902@naver.com>
2023-02-16 03:50:49 +0900

    HTTP 요청 파라미터 - @RequestParam( required=true/false , defaultValue = "value") 주의점, paramMap과 multiValueMap 사용

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -4,10 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -22,4 +25,58 @@ public class RequestParamController {
         response.getWriter().write("ok");
     }
 
+    @ResponseBody //@ResponseBody 추가 View 조회를 무시하고, HTTP message body 직접 해당 내용 입력
+    @GetMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge
+    ) {
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    @ResponseBody
+    @GetMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age
+    ) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @GetMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @GetMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age
+            //자바 int null이 들어 갈 수 없기 때문에 int로 하면 오류
+    ) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+    @ResponseBody
+    @GetMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age
+            //자바 int null이 들어 갈 수 없기 때문에 int로 하면 오류
+    ) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @GetMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username={},age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
 }
