7952ce7e70a637190d4bfba07efea3a68df3b2e3
bumgyu <soul1902@naver.com>
2023-02-19 17:12:39 +0900

    상품 수정- 타임리프

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -70,6 +70,21 @@ public class BasicItemController {
 //        model.addAttribute("item", item); //자동 추가, 생략 가능
         return "basic/item";
     }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String editForm(@PathVariable long itemId, Item item) {
+        itemRepository.update(itemId, item);
+
+        return "redirect:/basic/items/{itemId}";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
